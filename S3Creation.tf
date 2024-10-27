d9124509e0b58bcc6c13dd3540b04d0b450011e7
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-10-27 19:48:34 +0530

    Add files via upload

--- S3Creation.tf
+++ S3Creation.tf
@@ -1,9 +1,10 @@
-resource "aws_s3_bucket" "BucketCreate" {
-  bucket = "kesava-tf-log-bucket"
+resource "aws_s3_bucket" "MyBucket" {
+  bucket = "Kesava-TF-Bucket"  # Change to your unique bucket name
+  #acl    = "private"  # Access control list; options include "private", "public-read", etc.
+
   tags = {
-    name          = "kesava"
-    environment   = "Prod"
-    target_prefix = "log/"
+    Name        = "MyBucket"
+    Environment = "Prod"
   }
   versioning {
     enabled = true
