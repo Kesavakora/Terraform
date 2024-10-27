ab5d024e5fb794fe1404c52e7276156a9e4c0aa7
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-10-27 09:42:03 +0530

    Add files via upload

--- Keyname_Creation.tf
+++ Keyname_Creation.tf
@@ -14,21 +14,39 @@ resource "tls_private_key" "example" {
 resource "aws_key_pair" "generated_key" {
   key_name   = var.key_name
   public_key = tls_private_key.example.public_key_openssh
-}*/
+}
 
 resource "aws_key_pair" "tf-key-pair" {
   key_name   = "tf-key-pair"
-  public_key = var.keypair == true ? 0 : tls_private_key.rsa.public_key_openssh
- 
+  public_key = tls_private_key.rsa.public_key_openssh
 }
+
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
+}*/
+
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-resource "local_file" "tf-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair"
+
+# Try to find an existing key pair
+data "aws_key_pair" "existing" {
+  key_name = var.key_name
+  # This will attempt to read the key from AWS
 }
 
+# Conditionally create the key pair if it doesn't exist
+resource "aws_key_pair" "new_key_pair" {
+  count      = data.aws_key_pair.existing.id != "" ? 1 : 0
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+# Use the key pair by referring to either the existing data source or the new resource
+output "key_pair_id" {
+  value = data.aws_key_pair.existing.id != "" ? data.aws_key_pair.existing.id : aws_key_pair.new_key_pair[0].id
+}
 
 
--- Variables.tf
+++ Variables.tf
@@ -1,3 +1,11 @@
-variable "keypair" {
-  default = true
+variable "key_name" {
+  description = "The name of the key pair"
+  type        = string
+  default     = "tf-key-pair"
+}
+
+variable "public_key_path" {
+  description = "The path to the public key file"
+  type        = string
+  default     = tls_private_key.rsa.public_key_openssh
 }
