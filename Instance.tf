2b239a0c2dd33e8d2dd8085b5d00799ef558acd1
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-12 18:58:33 +0530

    Add files via upload

--- Instance.tf
+++ Instance.tf
@@ -30,7 +30,12 @@ resource "aws_instance" "Ubuntu_Instance" {
       "sudo sed -i 's/80/8080/' /etc/apache2/ports.conf",
       "sudo sed -i 's/*:80/*:8080/' /etc/apache2/sites-enabled/000-default.conf",
       "sudo ufw allow 8080",                  # Open the new port on the firewall
-      "sudo systemctl restart apache2"        # Restart Apache to apply changes
+      "sudo systemctl restart apache2",       # Restart Apache to apply changes
+      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+      "sudo apt update",
+      "sudo apt install -y docker-ce"
     ]
   }
 
