e760665aa772d6918d5e50231cbbfa8d7b822091
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-12 19:22:04 +0530

    Add files via upload

--- Instance.tf
+++ Instance.tf
@@ -33,8 +33,8 @@ resource "aws_instance" "Ubuntu_Instance" {
       "sudo systemctl restart apache2",       # Restart Apache to apply changes
       "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
-      "sudo apt update",
+      "sudo add-apt-repository -y deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+      "sudo apt update -y",
       "sudo apt install -y docker-ce"
     ]
   }
