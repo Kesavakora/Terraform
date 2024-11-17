811af8f4a4ae0eb6c1389b8d2af0c32ca786e97e
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-17 21:07:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,10 @@ output "ansible_inventory" {
   EOF
 }
 
-# Output the key pair ID based on whether it was created
+/*# Output the key pair ID based on whether it was created
 output "key_pair_id" {
   value = length(aws_key_pair.new_key_pair) > 0 ? aws_key_pair.new_key_pair.id : var.key_name
-}
+}*/
 
 output "lambda_function_arn" {
   value = aws_lambda_function.example_lambda.arn
