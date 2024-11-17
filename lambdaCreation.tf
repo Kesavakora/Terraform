35a8ba8a9b105cdef9a3dacf5de8474960b675b2
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-17 21:02:58 +0530

    Add files via upload

---
+++ lambdaCreation.tf
@@ -0,0 +1,42 @@
+
+# Step 1: Create an IAM Role for Lambda
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy" {
+  name       = "lambda-basic-policy-attachment"
+  roles      = [aws_iam_role.lambda_execution_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Step 2: Package the Lambda Function Code
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+# Step 3: Create the Lambda Function
+resource "aws_lambda_function" "example_lambda" {
+  function_name    = "example_lambda_function"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
+
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,17 @@
 output "ansible_inventory" {
   value = <<-EOF
     [webservers]
-    %{ for ip in aws_instance.Ubuntu_Instance.*.public_ip ~} >> "/etc/ansible/hosts"
+    %{for ip in aws_instance.Ubuntu_Instance.*.public_ip~} >> "/etc/ansible/hosts"
     ${ip}
-    %{ endfor ~}
+    %{endfor~}
   EOF
+}
+
+# Output the key pair ID based on whether it was created
+output "key_pair_id" {
+  value = length(aws_key_pair.new_key_pair) > 0 ? aws_key_pair.new_key_pair.id : var.key_name
+}
+
+output "lambda_function_arn" {
+  value = aws_lambda_function.example_lambda.arn
 }
