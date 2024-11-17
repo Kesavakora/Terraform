9a7f1542202d3af4b8ff64550a871cda62f6c666
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-17 22:37:11 +0530

    Update SecurityGroup.tf

--- SecurityGroup.tf
+++ SecurityGroup.tf
@@ -17,6 +17,20 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    from_port   = 8082
+    to_port     = 8082
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
