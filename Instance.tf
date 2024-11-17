3ce6a9d2fabf1d2ad8acf80d8b872fdd7afb32b1
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-17 21:13:26 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "Ubuntu_Instance" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file(local_file.tf-key[count.index].filename)
+    private_key = file(local_file.tf-key.filename)
     timeout     = "4m"
   }
 
@@ -58,7 +58,7 @@ resource "null_resource" "copy-test-file" {
     type        = "ssh"
     host        = aws_instance.Ubuntu_Instance[*].id
     user        = "ubuntu"
-    private_key = file(local_file.tf_key.id)
+    private_key = file(local_file.tf_key.filename)
   }
 }
 
