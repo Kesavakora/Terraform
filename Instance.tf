2173e5f87781c8ac0dd769460329c7c6eca4142f
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-22 20:57:20 +0530

    Update Instance.tf

--- Instance.tf
+++ Instance.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "Ubuntu_Instance" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ubuntu"
-    private_key = file(local_file.tf-key[0].filename)
+    private_key = file(local_file.tf_key.id)
     timeout     = "4m"
   }
 
