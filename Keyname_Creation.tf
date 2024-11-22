078ffd04bf030a620f74f5a088d4e7e34a3c8715
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-22 20:59:38 +0530

    Update Keyname_Creation.tf

--- Keyname_Creation.tf
+++ Keyname_Creation.tf
@@ -39,7 +39,7 @@ resource "tls_private_key" "rsa" {
 
 # Conditionally create the key pair based on the variable
 resource "aws_key_pair" "new_key_pair" {
-  count    = var.key_name ? 1: 0
+  count    = var.create_key_pair ? 1: 0
   key_name = var.key_name
   #public_key = file(var.public_key_path)
   public_key = file("~/.ssh/${var.key_name}")
@@ -47,7 +47,7 @@ resource "aws_key_pair" "new_key_pair" {
 }
 
 resource "local_file" "tf-key" {
-  #count    = var.key_name ? 1: 0
+  count    = var.create_key_pair ? 1: 0
   content  = tls_private_key.rsa.private_key_pem
   filename = "my-key-pair"
 }
