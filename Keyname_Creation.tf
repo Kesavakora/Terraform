6f7b7ba45f3b0532233918f15c2117568dce53fa
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-22 20:55:48 +0530

    Update Keyname_Creation.tf

--- Keyname_Creation.tf
+++ Keyname_Creation.tf
@@ -47,7 +47,7 @@ resource "aws_key_pair" "new_key_pair" {
 }
 
 resource "local_file" "tf-key" {
-  count    = var.key_name ? 1: 0
+  #count    = var.key_name ? 1: 0
   content  = tls_private_key.rsa.private_key_pem
   filename = "my-key-pair"
 }
