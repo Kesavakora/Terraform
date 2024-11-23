4cff172d55d3b72b69992d67d909d7f4ef6f811a
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-11-23 15:52:10 +0530

    Update Keyname_Creation.tf

--- Keyname_Creation.tf
+++ Keyname_Creation.tf
@@ -41,14 +41,14 @@ data "aws_key_pair" "existing" {
 # Conditionally create the key pair based on the variable
 resource "aws_key_pair" "new_key_pair" {
   #count    = file("~/.jenkins/workspace/EC2CreationTerraform/${var.key_name}") == "" ? 1 : 0
-  count = data.aws_key_pair.existing.id == "" ? 1 : 0
+  count = data.aws_key_pair.existing.id != "" ? 1 : 0
   key_name = "~/.jenkins/workspace/EC2CreationTerraform/${var.key_name}"
   public_key = file("~/.jenkins/workspace/EC2CreationTerraform/${var.key_name}")
 }
 
 resource "local_file" "tf-key" {
   #count    = file("~/.jenkins/workspace/EC2CreationTerraform/${var.key_name}") == 0 ? 1 : 0
-  count = data.aws_key_pair.existing.id == "" ? 1 : 0
+  count = data.aws_key_pair.existing.id != "" ? 1 : 0
   content  = tls_private_key.rsa.private_key_pem
   filename = "my-key-pair"
 }
