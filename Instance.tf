fc95a9f2f280b189c7633ab18ab679c95cc2afa9
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2024-12-12 05:40:26 +0530

    Add files via upload

---
+++ Instance.tf
@@ -0,0 +1,79 @@
+resource "aws_instance" "Ubuntu_Instance" {
+  ami           = "ami-0522ab6e1ddcc7055" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"              # Instance type
+  #key_name        = aws_key_pair.tf-key-pair.key_name # Associate the key pair with the EC2 instance
+
+  key_name        = var.key_name
+  security_groups = [aws_security_group.allow_ssh.name]
+  count           = 2
+  #provider = aws.Administ
+
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo install apache2 -y
+  sudo start apache2
+  echo "*** Completed Installing apache2"
+  EOF
+
+  provisioner "remote-exec" {
+    inline = [
+      "touch Hello.txt",
+      "echo Hello worlds  >> Hello.txt",
+      "sudo apt update -y",
+      "sudo apt install git -y",
+      "sudo apt install apache2 -y",
+      "sudo start apache2",
+      "sudo sed -i 's/80/8080/' /etc/apache2/ports.conf",
+      "sudo sed -i 's/*:80/*:8080/' /etc/apache2/sites-enabled/000-default.conf",
+      "sudo ufw allow 8080",            # Open the new port on the firewall
+      "sudo systemctl restart apache2", # Restart Apache to apply changes
+      "sudo apt install -y apt-transport-https ca-certificates curl software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+      "sudo add-apt-repository -y deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+      "sudo apt update -y",
+      "sudo apt install -y docker-ce"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file(local_file.tf-key.id)
+    timeout     = "4m"
+  }
+
+  tags = {
+    Name = "Kesava Instance"
+  }
+}
+
+resource "null_resource" "copy-test-file" {
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.Ubuntu_Instance[*].id
+    user        = "ubuntu"
+    private_key = file(local_file.tf_key.id)
+  }
+}
+
+resource "aws_ebs_encryption_by_default" "enabled" {
+  enabled = true
+}
+
+resource "null_resource" "ansible_hosts" {
+  provisioner "local-exec" {
+    command = <<EOT
+      # Clear the hosts file
+      echo "${var.sudo_password}" | sudo -S sh -c 'echo "" > /etc/ansible/hosts'
+      echo "[webservers]" | sudo -S tee -a /etc/ansible/hosts
+      echo "${join("\n", aws_instance.Ubuntu_Instance[*].public_ip)}" | sudo -S tee -a /etc/ansible/hosts
+    EOT
+  }
+  depends_on = [aws_instance.Ubuntu_Instance]
+}
---
+++ Keyname_Creation.tf
@@ -0,0 +1,56 @@
+/*# Create a new Key Pair
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"                  # The key pair name
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your local public key file
+}
+
+variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = var.key_name
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "aws_key_pair" "tf-key-pair" {
+  key_name   = "tf-key-pair"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "tf-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tf-key-pair"
+}*/
+
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Try to find an existing key pair
+/*data "aws_key_pair" "existing" {
+  key_name = var.key_name
+  # This will attempt to read the key from AWS
+}*/
+
+# Conditionally create the key pair based on the variable
+resource "aws_key_pair" "new_key_pair" {
+  count    = var.key_name ? 0 : 1
+  key_name = var.key_name
+  #public_key = file(var.public_key_path)
+  public_key = file("~/.ssh/${var.key_name}")
+  #public_key = tls_private_key.rsa.public_key_openssh
+}
+
+resource "local_file" "tf-key" {
+  count    = var.key_name ? 0 : 1
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "my-key-pair"
+}
+
+
+
