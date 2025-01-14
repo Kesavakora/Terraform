6da739628b34fbdc051f22cd9e606a206471b65d
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2025-01-14 10:58:38 +0530

    Add files via upload

--- RDS.tf
+++ RDS.tf
@@ -1,15 +1,14 @@
 resource "aws_db_instance" "myrds" {
-   allocated_storage   = 20
-   storage_type        = "gp2"
-   identifier          = "mydb"
-   engine              = "mysql"
-   engine_version      = "8.0.34"
-   instance_class      = "db.t2.micro"
-   username            = "admin"
-   password            = "Passw0rd!123"
-   publicly_accessible = true
-   skip_final_snapshot = true
-   parameter_group_name = "default.mysql8.0"        # Parameter group for MySQL 8.0
+   allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "8.0.34"
+  instance_class       = "db.t3.micro" # Updated instance class
+  username             = "admin"
+  password             = "adminpassword"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
    vpc_security_group_ids = [aws_security_group.allow_ssh.name]
 
 
