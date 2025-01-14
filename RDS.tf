d8797e878f0284990b0ab8fe155cee2a85a931d1
Kesavakora <45279276+Kesavakora@users.noreply.github.com>
2025-01-14 10:44:12 +0530

    Add files via upload

--- RDS.tf
+++ RDS.tf
@@ -3,12 +3,17 @@ resource "aws_db_instance" "myrds" {
    storage_type        = "gp2"
    identifier          = "mydb"
    engine              = "mysql"
-   engine_version      = "8.0.27"
+   engine_version      = "8.0.34"
    instance_class      = "db.t2.micro"
    username            = "admin"
    password            = "Passw0rd!123"
    publicly_accessible = true
    skip_final_snapshot = true
+   parameter_group_name = "default.mysql8.0"        # Parameter group for MySQL 8.0
+   skip_final_snapshot  = true                      # Avoid final snapshot on deletion
+   publicly_accessible  = true                      # Set to false for private access
+   vpc_security_group_ids = [aws_security_group.allow_ssh.name]
+
 
    tags = {
      Name = "MyRDS"
